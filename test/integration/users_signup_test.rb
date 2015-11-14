4008cedb5ce6c761fe8220d92e54b944f0035900
harhogefoo <harimanmon@gmail.com>
2015-11-14 16:29:30 -0500

    Finish Chapter 7.

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,6 +11,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
          password_confirmation: "bar" }
      end
      assert_template 'users/new'
+     assert_select 'div#error_explanation'
+     assert_select 'div.alert'
+     assert_select 'div.alert-danger'
    end
    test "valid signup information" do
      get signup_path
@@ -23,5 +26,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
        }
      end
      assert_template 'users/show'
+     assert_not flash.empty?
    end
 end
